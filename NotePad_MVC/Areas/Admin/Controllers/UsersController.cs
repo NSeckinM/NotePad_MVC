85fe5fd0674590ab6c87d5e7b23775a8fb7180d1
NseckinM <seckinmantar@gmail.com>
2021-09-14 01:47:46 +0300

    Admin layout edit

---
+++ NotePad_MVC/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotePad_MVC.Areas.Admin.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
