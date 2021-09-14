ad64e5990f7e5cd2693ffc5a94016a8fe549aa48
NseckinM <seckinmantar@gmail.com>
2021-09-14 03:38:50 +0300

    Added User block

--- NotePad_MVC/Areas/Admin/Controllers/UsersController.cs
+++ NotePad_MVC/Areas/Admin/Controllers/UsersController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NotePad_MVC.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +8,38 @@ using System.Threading.Tasks;
 
 namespace NotePad_MVC.Areas.Admin.Controllers
 {
+    [Area("Admin")]
+    [Authorize(Roles = "admin")]
     public class UsersController : Controller
     {
+        private readonly ApplicationDbContext db;
+
+        public UsersController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+
         public IActionResult Index()
         {
-            return View();
+            return View(db.Users.ToList());
+        }
+
+        [HttpPost]
+        public IActionResult ChangeStatus(string userId, bool active)
+        {
+
+            ApplicationUser user = db.Users.Find(userId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            user.LockoutEnabled = true;
+            user.LockoutEnd = active ? null : DateTimeOffset.MaxValue;
+            db.SaveChanges();
+
+
+            return Ok();
         }
     }
 }
