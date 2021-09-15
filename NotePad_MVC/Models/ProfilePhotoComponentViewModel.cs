ada553ebc4135c62a1592e6e9f98d7398c6ea30b
NseckinM <seckinmantar@gmail.com>
2021-09-15 13:33:51 +0300

    Added ViewComponent for ProfilePhoto

---
+++ NotePad_MVC/Models/ProfilePhotoComponentViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotePad_MVC.Models
+{
+    public class ProfilePhotoComponentViewModel
+    {
+        public string FileName { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string class1 { get; set; }
+
+
+    }
+}
---
+++ NotePad_MVC/ViewComponents/ProfilePhotoViewComponent.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NotePad_MVC.Data;
+using NotePad_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+
+namespace NotePad_MVC.ViewComponents
+{
+    //https://docs.microsoft.com/tr-tr/aspnet/core/mvc/views/view-components?view=aspnetcore-5.0
+    public class ProfilePhotoViewComponent: ViewComponent
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public ProfilePhotoViewComponent(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(int genislik, int yukseklik, string sinif)
+        {
+            var userId = ((ClaimsPrincipal)User).FindFirstValue(ClaimTypes.NameIdentifier);
+            var user = await userManager.FindByIdAsync(userId);
+
+            var vm = new ProfilePhotoComponentViewModel()
+            {
+                FileName = user.Photo,
+                Width = genislik,
+                Height = yukseklik,
+                class1 = sinif
+            };
+
+
+            return View(vm);
+        }
+
+
+    }
+}
